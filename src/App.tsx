8433a938fb36f4e3f0e5284bcff02494d5f32e7e
chaeyeonan <dkscodus1204@ewhain.net>
2024-01-30 00:15:17 +0900

    feat: make chips components

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,30 @@ import {
   PoopTypeButtons,
   CircleButtons,
 } from "./components/Buttons";
+import {
+  ColorType,
+  HashtagChips,
+  StatusChips,
+  StatusType,
+} from "./components/Chips";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Container>
+        {/* Buttons */}
         <BigButtons enable={false}>분석하기</BigButtons>
         <BigButtons enable={true}>저장하기</BigButtons>
         <PoopTypeButtons id={3}></PoopTypeButtons>
         <CircleButtons type="check" />
         <CircleButtons type="plus" />
+        {/* chips */}
+        <HashtagChips color={ColorType.MainOrange}>Button</HashtagChips>
+        <HashtagChips color={ColorType.SubOrange}>Button</HashtagChips>
+        <HashtagChips color={ColorType.Gray}>Button</HashtagChips>
+        <StatusChips statusType={StatusType.Good} />
+        <StatusChips statusType={StatusType.Soso} />
+        <StatusChips statusType={StatusType.Bad} />
       </Container>
     </ThemeProvider>
   );
---
+++ src/components/Chips/Chips.tsx
@@ -0,0 +1,98 @@
+import { styled } from "styled-components";
+import { theme } from "../../styles";
+import { Text } from "../common";
+
+export enum ChipType {
+  Hashtag = "Hashtag",
+  Status = "Status",
+}
+export enum ColorType {
+  MainOrange = "MainOrange",
+  SubOrange = "SubOrange",
+  Gray = "Gray",
+}
+export enum StatusType {
+  Good = "GOOD",
+  Soso = "SOSO",
+  Bad = "BAD",
+}
+
+export const HashtagChips = ({
+  children,
+  color,
+}: {
+  children: React.ReactNode;
+  color: ColorType;
+}) => {
+  let backgroundColor = "#000";
+  if (color === ColorType.MainOrange)
+    backgroundColor = theme.palette.Main_orange;
+  else if (color === ColorType.SubOrange) backgroundColor = theme.palette.Gray1;
+  else backgroundColor = theme.palette.Gray1;
+
+  return (
+    <Container
+      background={backgroundColor}
+      type={ChipType.Hashtag}
+      color={color}
+    >
+      <Text
+        $paletteColor={
+          color === ColorType.MainOrange
+            ? "White"
+            : color === ColorType.SubOrange
+            ? "Main_orange"
+            : "Gray9"
+        }
+        $Typo="Body4"
+      >
+        {children}
+      </Text>
+    </Container>
+  );
+};
+
+export const StatusChips = ({ statusType }: { statusType: StatusType }) => {
+  let backgroundColor = "#000";
+
+  if (statusType === StatusType.Good) backgroundColor = theme.palette.Blue2;
+  else if (statusType === StatusType.Soso)
+    backgroundColor = theme.palette.Gray3;
+  else if (statusType === StatusType.Bad) backgroundColor = theme.palette.Red2;
+
+  return (
+    <Container background={backgroundColor} statusType={statusType}>
+      <Text
+        $Typo="Body1"
+        $paletteColor={
+          statusType === StatusType.Bad
+            ? "Red"
+            : statusType === StatusType.Soso
+            ? "Gray6"
+            : "Blue"
+        }
+      >
+        {statusType}
+      </Text>
+    </Container>
+  );
+};
+
+const Container = styled.div<{
+  type?: ChipType;
+  color?: ColorType;
+  background: string;
+  statusType?: StatusType;
+}>`
+  width: ${({ type }) => (type === ChipType.Hashtag ? 76 : 54)}px;
+  height: ${({ type }) => (type === ChipType.Hashtag ? 36 : 22)}px;
+  background: ${({ background }) => (background ? background : "#000")};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: ${({ type }) => (type === ChipType.Hashtag ? 20 : 14)}px;
+  border: ${({ color }) =>
+    color === ColorType.SubOrange
+      ? `1px solid ${theme.palette.Main_orange}`
+      : `0px solid ${theme.palette.Main_orange}`};
+`;
