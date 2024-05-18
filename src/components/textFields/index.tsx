b953f786474a6d80218c7000ab04ed297e1dfa15
Chaeyeon An <dkscodus1204@ewhain.net>
2024-05-18 14:24:00 +0900

    Feat: 음식 선택 fast api 호출

--- src/components/textFields/index.tsx
+++ src/components/textFields/index.tsx
@@ -1,38 +1,105 @@
-import styled from 'styled-components'
-import { theme } from '../../styles'
-import { PlaneIcon } from '../../assets/Icons'
-import useInput from '../../hooks/useInput'
+import styled from "styled-components";
+import { theme } from "../../styles";
+import { PlaneIcon } from "../../assets/Icons";
+import useInput from "../../hooks/useInput";
+import { useSetRecoilState, useRecoilValue } from "recoil";
+import {
+  chatList,
+  isChooseFoodActive,
+  userAccessToken,
+} from "../../store/recoil";
+import { SERVER_AI } from "../../network/config";
 
 export enum InputType {
-  MenuInput = 'MenuInput',
-  ChatInput = 'ChatInput'
+  MenuInput = "MenuInput",
+  ChatInput = "ChatInput",
 }
 
-export const TextFields = ({ value, type, enable }: { value?: string; type: InputType; enable?: boolean }) => {
-  const { text, handleChange, resetText } = useInput('')
-  let placeholder = 'placeholder'
+export const TextFields = ({
+  value,
+  type,
+  enable,
+}: {
+  value?: string;
+  type: InputType;
+  enable?: boolean;
+}) => {
+  const { text, handleChange, resetText } = useInput("");
+  const setChatting = useSetRecoilState(chatList);
+  const setChooseFoodActive = useSetRecoilState(isChooseFoodActive);
+  const accessToken = useRecoilValue(userAccessToken);
 
-  if (type === InputType.ChatInput) placeholder = 'AI에게 무엇이든 요청하세요.'
-  else if (type === InputType.MenuInput) placeholder = '음식 입력'
+  let placeholder = "placeholder";
+
+  function onFoodListSend(e: any) {
+    setChooseFoodActive(true);
+    e.preventDefault();
+    setChatting((prevChatting) => [
+      ...prevChatting,
+      {
+        chatId: 12654,
+        senderId: 1,
+        receiverId: 0,
+        text: text,
+      },
+    ]);
+    resetText();
+
+    fetch(`${SERVER_AI}/choice`, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        content: text,
+      }),
+    })
+      // .then((res) => res.json())
+      .then((res) => console.log(res));
+  }
+
+  if (type === InputType.ChatInput) placeholder = "AI에게 무엇이든 요청하세요.";
+  else if (type === InputType.MenuInput) placeholder = "음식 입력";
   return (
-    <Container type={type} enable={enable}>
-      <input type="text" placeholder={placeholder} value={type === InputType.ChatInput ? text : value} onChange={handleChange} />
-      {type === InputType.ChatInput ? <PlaneIcon /> : null}
+    <Container
+      type={type}
+      enable={enable}
+      onSubmit={type === InputType.ChatInput ? onFoodListSend : undefined}
+    >
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={type === InputType.ChatInput ? text : value}
+        onChange={handleChange}
+      />
+      {type === InputType.ChatInput ? (
+        <CustomBtn type="submit">
+          <PlaneIcon />
+        </CustomBtn>
+      ) : null}
     </Container>
-  )
-}
+  );
+};
 
-const Container = styled.div<{ type: InputType; enable?: boolean }>`
+const Container = styled.form<{ type: InputType; enable?: boolean }>`
   display: flex;
   align-items: center;
   justify-content: space-between;
   box-sizing: border-box;
   width: 100%;
   height: ${({ type }) => (type === InputType.ChatInput ? 44 : 39)}px;
-  border: ${({ type, enable }) => (type === InputType.MenuInput ? (enable ? `1px solid ${theme.palette.Main_orange}` : `1px solid ${theme.palette.Gray4}`) : '0px')};
+  border: ${({ type, enable }) =>
+    type === InputType.MenuInput
+      ? enable
+        ? `1px solid ${theme.palette.Main_orange}`
+        : `1px solid ${theme.palette.Gray4}`
+      : "0px"};
   border-radius: ${({ type }) => (type === InputType.ChatInput ? 45 : 40)}px;
-  padding: ${({ type }) => (type === InputType.ChatInput ? '0 24px' : '0 16px')};
-  background: ${({ type }) => (type === InputType.ChatInput ? theme.palette.Gray1 : 'none')};
+  padding: ${({ type }) =>
+    type === InputType.ChatInput ? "0 24px" : "0 16px"};
+  background: ${({ type }) =>
+    type === InputType.ChatInput ? theme.palette.Gray1 : "none"};
 
   input {
     width: 80%;
@@ -44,11 +111,23 @@ const Container = styled.div<{ type: InputType; enable?: boolean }>`
     outline: none;
     border: none;
     border-radius: 40px;
-    color: ${({ enable }) => (enable ? theme.palette.Main_orange : theme.palette.Gray7)};
-    background: ${({ type }) => (type === InputType.ChatInput ? theme.palette.Gray1 : 'none')};
+    color: ${({ enable }) =>
+      enable ? theme.palette.Main_orange : theme.palette.Gray7};
+    background: ${({ type }) =>
+      type === InputType.ChatInput ? theme.palette.Gray1 : "none"};
 
     &::placeholder {
-      color: ${({ type, enable }) => (type === InputType.MenuInput ? (enable ? theme.palette.Main_orange : theme.palette.Gray4) : theme.palette.Gray7)};
+      color: ${({ type, enable }) =>
+        type === InputType.MenuInput
+          ? enable
+            ? theme.palette.Main_orange
+            : theme.palette.Gray4
+          : theme.palette.Gray7};
     }
   }
-`
+`;
+
+const CustomBtn = styled.button`
+  border: none;
+  background: ${theme.palette.Gray1};
+`;
--- src/pages/chat/chatting.tsx
+++ src/pages/chat/chatting.tsx
@@ -1,79 +1,90 @@
-import styled from 'styled-components'
-import { ChatBubble } from '../../components/chat/ChatBubble'
-import { palette } from '../../styles'
-import { Text } from '../../components/common'
-import { chooseFood, fisrtMessage, recommendDiet } from '../../store/messages'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { chatList, isChatPageActive, userAccessToken } from '../../store/recoil'
-import { useEffect, useRef, useState } from 'react'
-import { SERVER } from '../../network/config'
+import styled from "styled-components";
+import { ChatBubble } from "../../components/chat/ChatBubble";
+import { palette } from "../../styles";
+import { Text } from "../../components/common";
+import { chooseFood, fisrtMessage, recommendDiet } from "../../store/messages";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  chatList,
+  isChatPageActive,
+  isChooseFoodActive,
+  userAccessToken,
+} from "../../store/recoil";
+import { useEffect, useRef } from "react";
+import { SERVER } from "../../network/config";
 
 export const Chatting = () => {
-  const [chatting, setChatting] = useRecoilState(chatList)
-  const accessToken = useRecoilValue(userAccessToken)
-  const isInChatPage = useRecoilValue(isChatPageActive)
-  const [answer, setAnswer] = useState('')
+  const [chatting, setChatting] = useRecoilState(chatList);
+  const accessToken = useRecoilValue(userAccessToken);
+  const isInChatPage = useRecoilValue(isChatPageActive);
+  const [chooseFoodActive, setChooseFoodActive] =
+    useRecoilState(isChooseFoodActive);
 
-  const scrollRef = useRef<HTMLDivElement>(null)
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   const scrollBottom = () => {
     if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
     }
-  }
+  };
 
   useEffect(() => {
-    if (isInChatPage && chatting.at(-1) !== fisrtMessage) setChatting(prevChatting => [...prevChatting, fisrtMessage])
-  }, [isInChatPage])
+    if (isInChatPage && chatting.at(-1) !== fisrtMessage)
+      setChatting((prevChatting) => [...prevChatting, fisrtMessage]);
+  }, [isInChatPage]);
 
   useEffect(() => {
-    scrollBottom()
-  }, [chatting])
+    scrollBottom();
+  }, [chatting]);
 
   const getRecommendationsResult = async () => {
     await fetch(`${SERVER}/recommendations`, {
-      method: 'post',
+      method: "post",
       headers: {
-        'content-type': 'application/json',
-        Authorization: `Bearer ${accessToken}`
-      }
+        "content-type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
     })
-      .then(res => res.json())
-      .then(res =>
-        setChatting(prevChatting => [
+      .then((res) => res.json())
+      .then((res) =>
+        setChatting((prevChatting) => [
           ...prevChatting,
           {
             chatId: 12654,
             senderId: 0,
             receiverId: 1,
-            text: res.data[0].split('\n').map((content: string) => <span>{content}</span>)
-          }
+            text: res.data[0]
+              .split("\n")
+              .map((content: string) => <span>{content}</span>),
+          },
         ])
-      )
-  }
+      );
+  };
 
   const addChooseFoodChat = () => {
-    setChatting(prevChatting => [...prevChatting, chooseFood[0]])
+    setChooseFoodActive(true);
+    setChatting((prevChatting) => [...prevChatting, chooseFood[0]]);
 
     chooseFood.slice(1).forEach((message, index) => {
       setTimeout(() => {
-        setChatting(prevChatting => [...prevChatting, message])
-      }, (index + 1) * 500) // 0.5초 간격
-    })
-  }
+        setChatting((prevChatting) => [...prevChatting, message]);
+      }, (index + 1) * 500); // 0.5초 간격
+    });
+  };
 
   return (
     <Container ref={scrollRef}>
-      {chatting.map(message => (
+      {chatting.map((message) => (
         <ChatBubbleList chatId={message.chatId} senderId={message.senderId}>
           <ChatBubble>{message.text}</ChatBubble>
           {message.chatId === 1 && (
             <div className="chatChips_container">
               <ChatChip
                 onClick={() => {
-                  setChatting([...chatting, recommendDiet])
-                  getRecommendationsResult()
-                }}>
+                  setChatting([...chatting, recommendDiet]);
+                  getRecommendationsResult();
+                }}
+              >
                 <Text $Typo="Body3" $paletteColor="Main_orange">
                   식단 추천
                 </Text>
@@ -88,28 +99,32 @@ export const Chatting = () => {
         </ChatBubbleList>
       ))}
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
   width: 100%;
   overflow-y: auto;
-  padding: 18px 0 0 0;
+  padding: 18px 0 10px 0;
   height: calc(100vh - 345px);
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 
   .chatChips_container {
     display: flex;
     gap: 8px;
   }
-`
+`;
 
 const ChatBubbleList = styled.div<{ chatId?: number; senderId: number }>`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: ${({ chatId }) => (chatId === 1 ? 10 : 30)}px;
-  align-items: ${({ senderId }) => (senderId === 1 ? 'flex-end' : 'flex-start')};
-`
+  align-items: ${({ senderId }) =>
+    senderId === 1 ? "flex-end" : "flex-start"};
+`;
 
 const ChatChip = styled.button`
   width: fit-content;
@@ -117,4 +132,4 @@ const ChatChip = styled.button`
   border-radius: 20px;
   border: 1px solid ${palette.Main_orange};
   padding: 8px 16px;
-`
+`;
