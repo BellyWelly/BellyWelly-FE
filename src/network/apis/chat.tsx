4030f166bc6c7842131c5c10809d2c6f1a063261
Chaeyeon An <dkscodus1204@ewhain.net>
2024-06-20 22:35:44 +0900

    Fix: 원복

--- src/network/apis/chat.tsx
+++ src/network/apis/chat.tsx
@@ -1,15 +1,15 @@
-import { SERVER } from '../config'
+import { SERVER } from "../config";
 
 export const postDietRecommendation = async (accessToken: string) => {
   const res = await fetch(`${SERVER}/recommendations`, {
-    method: 'post',
+    method: "post",
     headers: {
-      'content-type': 'application/json',
-      Authorization: `Bearer ${accessToken}`
-    }
+      "content-type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
   })
-    .then(res => res.json())
-    .catch(error => console.error('Error:', error))
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error));
 
-  return res
-}
+  return res;
+};
